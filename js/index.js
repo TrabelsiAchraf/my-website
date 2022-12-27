f259ccfdbe53f9607e2607d18058d7fd48659c33
a.trabelsi <a.trabelsi@oodrive.com>
2022-12-27 16:08:51 +0100

    download pdf resume file when clicking on resume navbar link

--- js/index.js
+++ js/index.js
@@ -2,8 +2,3 @@
 // Get full year date for copyright text
 var datetime = new Date().getFullYear();
 document.getElementById("year").textContent = datetime;
-
-// Switch frames
-function showResumeFrame() {
-    document.getElementById('container').innerHTML = '<iframe src="files/CV_Achraf_Trabelsi.pdf" frameborder="0" width="100%" height="800"></iframe>';
-}
